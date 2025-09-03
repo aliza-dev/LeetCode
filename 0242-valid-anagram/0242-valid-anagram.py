210757499907f4ee59de200c2120e571a6e1d33a
Aliza Tariq <alizait1192@gmail.com>
2025-09-03 22:27:22 +0500

    Time: 16 ms (24.81%), Space: 17.9 MB (64.63%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # return sorted(s) == sorted(t)
+        if len(s) != len(t):   
+            return False
+        count={}
+        for i in s:
+            if i in count:
+                count[i] += 1
+            else:
+                count[i] = 1
+        for i in t:
+            if i not in count:
+                return False
+            count[i] -= 1
+            if count[i]<0:
+                return False
+        for val in count.values():
+            if val == 0:
+                return True
+        return False
+            
