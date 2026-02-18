c18282c8daba356d46efd542e542ebbffc07b2da
Aliza Tariq <alizait1192@gmail.com>
2026-02-18 12:02:25 +0500

    Time: 0 ms (100%), Space: 26.6 MB (32.65%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,11 +1,7 @@
-class Solution(object):
-    def rotate(self, nums, k):
-        k = k % len(nums)
-        def helper(left, right):
-            while left < right:
-                nums[left], nums[right] = nums[right], nums[left]
-                left += 1
-                right -= 1
-        helper(0, len(nums)-1)
-        helper(0, k-1)
-        helper(k, len(nums)-1 )        
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+      n = len(nums)
+      k = k % n
+
+      nums[:] = nums[n-k:] + nums[:n-k]
+        
